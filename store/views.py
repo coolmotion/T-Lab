1c6bd3ce33e9fd3834e84021763fb27047b08453
coolmotion <hcnpeiris@gmail.com>
2024-04-15 13:46:38 +0530

    fix product options

--- store/views.py
+++ store/views.py
@@ -26,7 +26,7 @@ def login_user(request):
         if user is not None:
             login(request, user)
             messages.success(request, ("You have been logged in!"))
-            return redirect('home')
+            return redirect('shop')
         else:
             messages.success(request, ("There was an error. Please try again"))
     else:
@@ -36,7 +36,7 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     messages.success(request, ('You have been logged out'))
-    return redirect('home')
+    return redirect('shop')
 
 
 def register_user(request):
