d98412ceb9cb0f9210eec9ffacdd01e1bde15db1
coolmotion <hcnpeiris@gmail.com>
2024-04-17 18:06:26 +0530

    set up ptest page

--- payment/urls.py
+++ payment/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('payment_sucess', views.payment_sucess, name="payment_sucess"),
     path('checkout', views.checkout, name="checkout"),
+    path('test', views.paymenttest, name="test"),
 ]
--- payment/views.py
+++ payment/views.py
@@ -7,3 +7,31 @@ def payment_sucess(request):
 
 def checkout(request):
     return render(request, "payment/checkout.html", {})
+
+
+def paymenttest(request):
+    data_for_payment = {
+        "sandbox": True,
+        "merchant_id": "1223860",
+        "return_url": None,
+        "cancel_url": None,
+        "notify_url": "http://sample.com/notify",
+        "order_id": "ItemNo12345",
+        "items": "Door bell wireless",
+        "amount": "1000.00",
+        "currency": "LKR",
+        "hash": "45D3CBA93E9F2189BD630ADFE19AA6DC",
+        "first_name": "Saman",
+        "last_name": "Perera",
+        "email": "samanp@gmail.com",
+        "phone": "0771234567",
+        "address": "No.1, Galle Road",
+        "city": "Colombo",
+        "country": "Sri Lanka",
+        "delivery_address": "No. 46, Galle road, Kalutara South",
+        "delivery_city": "Kalutara",
+        "delivery_country": "Sri Lanka",
+        "custom_1": "",
+        "custom_2": ""
+    }
+    return render(request, "payment/ptest.html", {'payment_data': data_for_payment})
