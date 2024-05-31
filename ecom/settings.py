62919c50aa50a1dbe0ac8028b23a6134707d2bf2
coolmotion <hcnpeiris@gmail.com>
2024-05-31 14:14:54 +0530

    stattic'

--- ecom/settings.py
+++ ecom/settings.py
@@ -109,9 +109,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
-STATIC_ROOT = 'allstatic/'
+STATIC_URL = 'allstatic/'
+STATICFILES_DIRS = ['allstatic/']
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
