6f6351d74584c972549f11ad34d4be63c69cfbae
coolmotion <hcnpeiris@gmail.com>
2024-06-04 08:50:12 +0530

    fix payment issues

--- payment/urls.py
+++ payment/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('payment_sucess', views.payment_sucess, name="payment_sucess"),
     path('checkout', views.checkout, name="checkout"),
     path('billing_info', views.billing_info, name="billing_info"),
-    path('pay_now', views.pay_now, name="pay_now"),
 ]
